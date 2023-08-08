2c82a08e890304184a308db262f627c0586b309d
yeoncheong08 <yeoncheong08@gmail.com>
2023-08-08 19:50:07 +0900

    feat: database model areacode

--- Firstproject/shelter/models.py
+++ Firstproject/shelter/models.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+
 from django.db import models
 
-# Create your models here.
+class AreaCode(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    def __str__(self):return self.code
